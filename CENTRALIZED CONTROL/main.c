fcdb43be9f0932e0e38e8b5e0c97b9adfcf08f13
AnNguyen2332 <truongan2332@gmail.com>
2021-12-03 00:01:32 +0700

    Centralized control

---
+++ CENTRALIZED CONTROL/main.c
@@ -0,0 +1,107 @@
+/*
+ * File:   main.c
+ * Author: Nguyen Truong An
+ *
+ * Created on December 2, 2021, 9:44 PM
+ */
+
+
+#pragma config FOSC = XT   // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF  // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF   // Low-Voltage In-Circuit Serial Programming Enable bit
+#pragma config CPD = OFF   // Data EEPROM Memory Code Protection bit 
+#pragma config WRT = OFF   // Flash Program Memory Write Enable bits 
+#pragma config CP = OFF    // Flash Program Memory Code Protection bit
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <xc.h>
+#include <pic16f877a.h>
+#define _XTAL_FREQ 20000000
+#include "uart.h"
+#include "pwm.h"
+
+char e = 0, prev_e = 0;
+char P = 0, I = 0, D = 0;
+char PID_Val = 0;
+
+float Kp = 0.8;
+float Ki = 0.6;
+float Kd = 0;
+
+int v_ref = 1250;
+
+void ADC_Init() {
+    ADCON0 = 0b1000001;
+    ADCON1 = 0b11000000;
+}
+
+unsigned int ADC_Read(unsigned char channel) {
+    ADCON0 &= 0x11000101;
+    ADCON0 |= channel << 3;
+    __delay_ms(2);
+    GO_nDONE = 1;
+    while (GO_nDONE);
+    return ((ADRESH << 8) + ADRESL);
+}
+
+void AVG_Index() {
+    unsigned char x0, x1, x2, x3, x4;
+    char uart_logs[50];
+
+    x0 = ADC_Read(0);
+    x1 = ADC_Read(1);
+    x2 = ADC_Read(2);
+    x3 = ADC_Read(3);
+    x4 = ADC_Read(4);
+
+    e = 17 * ((-2) * x0 + (-1) * x1 + 0 * x2 + 1 * x3 + 2 * x4) / (x0 + x1 + x2 + x3 + x4);
+
+    sprintf(uart_logs, "%d %d %d %d %d\n\r", x0, x1, x2, x3, x4);
+    UART_Write_Text(uart_logs);
+    __delay_ms(50);
+    sprintf(uart_logs, "e = %d\n\r", e);
+    UART_Write_Text(uart_logs);
+    __delay_ms(50);
+}
+
+void PID() {
+    char uart_logs[50];
+    P = e;
+    I = I + e;
+    D = e - prev_e;
+    PID_Val = Kp * P + Ki * I + Kd*D;
+    prev_e = e;
+    sprintf(uart_logs, "PID_Val = %d\n\r", PID_Val);
+    UART_Write_Text(uart_logs);
+}
+
+void main() {
+    TRISA = 1; //PORTA as input
+    TRISD = 0; //PORTD as output
+
+    PWM1_Init_Fre(1000);
+    PWM2_Init_Fre(1000);
+
+    PORTDbits.RD1 = 0;
+    PORTDbits.RD2 = 1;
+    PORTDbits.RD3 = 0;
+    PORTDbits.RD4 = 1;
+
+    ADC_Init();
+    UART_Init(9600);
+
+    while (1) {
+        AVG_Index();
+        PID();
+        PWM1_Duty(v_ref - PID_Val);
+        PWM2_Duty(v_ref + PID_Val);
+        PWM1_Start();
+        PWM2_Start();
+        __delay_ms (500);
+    }
+}
